fa8b8ac3e6d0892747110d592288fed9e2959539
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-06 20:41:01 -0600

    Tipos de actividades en administrador

--- electivapp/apps/actividades/admin.py
+++ electivapp/apps/actividades/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TipoActividad
 
 # Register your models here.
+@admin.register(TipoActividad)
+class AdminTipoActividad(admin.ModelAdmin):
+    list_per_page = 10000000000
