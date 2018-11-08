aa94eda054a11e1a0de18460e33b5cb26c4afd9a
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-08 00:07:23 -0600

    Urls y formularios para eliminar tipo de actividad.

--- electivapp/apps/actividades/urls.py
+++ electivapp/apps/actividades/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import TiposActividadListView, TiposActividadFormView, TiposActividadUpdateView
+from .views import TiposActividadListView, TiposActividadFormView, TiposActividadUpdateView, TiposActividadDeleteView
 
 app_name = "actividades"
 urlpatterns = [
     path("tipos", view=TiposActividadListView.as_view(), name="lista_tipos"),
     path("tipos/crear", view=TiposActividadFormView.as_view(), name="nuevo_tipo"),
     path("tipos/modificar/<str:pk>", view=TiposActividadUpdateView.as_view(), name="modificar_tipo"),
+    path("tipos/eliminar/<str:pk>", view=TiposActividadDeleteView.as_view(), name="eliminar_tipo"),
 ]
--- electivapp/apps/actividades/views.py
+++ electivapp/apps/actividades/views.py
@@ -9,6 +9,11 @@ from .forms import TipoActividadForm
 
 # Create your views here.
 
+class TiposActividadListView(LoginRequiredMixin, ListView):
+    template_name = 'actividades/tipoactividad_list.html'
+    model = TipoActividad
+    context_object_name = 'tipos'
+
 class TiposActividadFormView(LoginRequiredMixin, FormView):
     template_name = 'actividades/tipoactividad_form.html'
     form_class = TipoActividadForm
@@ -28,15 +33,11 @@ class TiposActividadUpdateView(LoginRequiredMixin, UpdateView):
         form.save()
         return HttpResponseRedirect(self.get_success_url())
 
-class TiposActividadListView(LoginRequiredMixin, ListView):
-    template_name = 'actividades/tipoactividad_list.html'
+class TiposActividadDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = 'actividades/tipoactividad_confirm_delete.html'
     model = TipoActividad
-    context_object_name = 'tipos'
-
-class TiposActividadDeleteView(LoginRequiredMixin, ListView):
-    template_name = 'account/signup.html'
-    model = TipoActividad
-    context_object_name = 'tipos'
+    context_object_name = 'tipo'
+    success_url = reverse_lazy('actividades:lista_tipos')
 
 class Test(FormView):
     template_name = 'users/user_form.html'
