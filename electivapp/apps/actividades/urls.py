b2d13dfefc5b87be6347916b84c37c30e06b37f8
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-08 14:17:55 -0600

    VIsta base para registro de actividad.

--- electivapp/apps/actividades/urls.py
+++ electivapp/apps/actividades/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import TiposActividadListView, TiposActividadFormView, TiposActividadUpdateView, TiposActividadDeleteView
+from .views import TiposActividadListView, TiposActividadFormView, TiposActividadUpdateView, TiposActividadDeleteView, RegistrarActividadView
 
 app_name = "actividades"
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path("tipos/crear", view=TiposActividadFormView.as_view(), name="nuevo_tipo"),
     path("tipos/modificar/<str:pk>", view=TiposActividadUpdateView.as_view(), name="modificar_tipo"),
     path("tipos/eliminar/<str:pk>", view=TiposActividadDeleteView.as_view(), name="eliminar_tipo"),
+    path("registrar", view=RegistrarActividadView.as_view(), name="registrar"),
 ]
--- electivapp/apps/actividades/views.py
+++ electivapp/apps/actividades/views.py
@@ -1,10 +1,10 @@
 from django.urls import reverse_lazy
-from django.views.generic import FormView, UpdateView, DeleteView, ListView
+from django.views.generic import TemplateView, FormView, UpdateView, DeleteView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 
-from .models import TipoActividad
+from .models import TipoActividad, Actividad
 from .forms import TipoActividadForm
 
 # Create your views here.
@@ -38,6 +38,12 @@ class TiposActividadDeleteView(LoginRequiredMixin, DeleteView):
     model = TipoActividad
     context_object_name = 'tipo'
     success_url = reverse_lazy('actividades:lista_tipos')
+
+class RegistrarActividadView(LoginRequiredMixin, TemplateView):
+    template_name = 'actividades/actividad_registrar.html'
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('holi')
 
 class Test(FormView):
     template_name = 'users/user_form.html'
