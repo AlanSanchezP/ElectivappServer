9f39837392033941459f0f074411c7d67a39bfc6
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-07 23:31:30 -0600

    Formulario de modificar tipo de actividad y traducciones modificadas.

--- electivapp/apps/actividades/forms.py
+++ electivapp/apps/actividades/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 from .models import TipoActividad
 
 class TipoActividadForm(forms.ModelForm):
     class Meta:
         model = TipoActividad
         exclude = ['']
+        labels = {
+            'valorHoras': _('Value per hour'),
+        }
