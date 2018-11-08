932d237f7e63468ba28943a8c59522384ae24b40
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-08 00:02:23 -0600

    Modificado modal para ser un formulario.

--- electivapp/static/js/project.js
+++ electivapp/static/js/project.js
@@ -2,7 +2,7 @@
 (function() {
     $('.delete-button').click(function(e) {
         var href = e.target.dataset.href;
-        $('#modal_action_button').attr('href', href);
+        $('#modal_action_form').attr('action', href);
         $('#delete_modal').modal();
     });
 })();
