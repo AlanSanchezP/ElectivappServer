33b22872bada3c40552392f5bee24e5a8a3e69df
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-07 23:51:39 -0600

    Bootstrap modal prepared for delete view.

--- electivapp/static/js/project.js
+++ electivapp/static/js/project.js
@@ -1,1 +1,8 @@
 /* Project specific Javascript goes here. */
+(function() {
+    $('.delete-button').click(function(e) {
+        var href = e.target.dataset.href;
+        $('#modal_action_button').attr('href', href);
+        $('#delete_modal').modal();
+    });
+})();
