c5a8b7ce698073e99ee9f4c07580d5bfafb020f3
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-08 20:51:27 -0600

    Lista de responsables (incompleta por urls de acciones)

--- electivapp/apps/alumnos/urls.py
+++ electivapp/apps/alumnos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from electivapp.apps.alumnos.views import AlumnosListView
+from electivapp.apps.alumnos.views import AlumnosListView, ResponsablesListView
 
 app_name = "alumnos"
 urlpatterns = [
     path("consulta", view=AlumnosListView.as_view(), name="lista"),
+    path("responsables", view=ResponsablesListView.as_view(), name="lista"),
 ]
--- electivapp/apps/alumnos/views.py
+++ electivapp/apps/alumnos/views.py
@@ -4,10 +4,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 
-from .models import Alumno
+from .models import Alumno, Responsable
 
 # Create your views here.
 class AlumnosListView(LoginRequiredMixin, TemplateView):
     template_name = 'alumnos/alumno_list.html'
     # model = Alumno
     # context_object_name = 'alumno'
+
+class ResponsablesListView(LoginRequiredMixin, ListView):
+    template_name = 'alumnos/responsable_list.html'
+    model = Responsable
+    context_object_name = 'responsables'
