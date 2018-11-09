b3959bb99615edf16c02eb06230e4c0f20708d95
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-08 20:53:16 -0600

    Corregidos botones de volver y de menu de responsables.

--- electivapp/apps/alumnos/urls.py
+++ electivapp/apps/alumnos/urls.py
@@ -5,5 +5,5 @@ from electivapp.apps.alumnos.views import AlumnosListView, ResponsablesListView
 app_name = "alumnos"
 urlpatterns = [
     path("consulta", view=AlumnosListView.as_view(), name="lista"),
-    path("responsables", view=ResponsablesListView.as_view(), name="lista"),
+    path("responsables", view=ResponsablesListView.as_view(), name="lista_responsables"),
 ]
