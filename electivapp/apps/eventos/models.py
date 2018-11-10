8086f869298400aa886d9cef0c74ad3f7c764bfe
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-09 20:47:46 -0600

    Añadido campo nombre de evento

--- electivapp/apps/eventos/models.py
+++ electivapp/apps/eventos/models.py
@@ -4,6 +4,10 @@ from electivapp.apps.alumnos.models import Responsable
 
 # Create your models here.
 class EventoAuditorio(models.Model):
+    nombre = models.CharField(
+        max_length=200
+    )
+
     fecha = models.DateTimeField()
 
     duracion = models.DurationField()
