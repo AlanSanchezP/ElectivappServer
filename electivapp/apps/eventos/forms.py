87a20075465d7d4eb1531f112c3241af4befb8b9
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-10 23:15:18 -0600

    Cambio en form de evento.

--- electivapp/apps/eventos/forms.py
+++ electivapp/apps/eventos/forms.py
@@ -7,3 +7,10 @@ class EventoForm(forms.ModelForm):
     class Meta:
         model = EventoAuditorio
         exclude = ['validado']
+        widgets = {
+            'fecha': forms.DateTimeInput(format='%d/%m/%Y %H:%M')
+        }
+        labels = {
+            'fecha': _('Fecha (dd/mm/aaaa HH:MM)'),
+            'duracion': _('Duración (HH:MM:ss)')
+        }
