d14c2840d37892c9d38f3ef14ba69126f5944343
Alan Sanchez <sanchezpineda03@gmail.com>
2018-11-11 22:47:03 -0600

    Campo ID en API de eventos.

--- electivapp/apps/eventos/serializers.py
+++ electivapp/apps/eventos/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
 class EventoSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     nombre = serializers.CharField()
     fecha = serializers.DateTimeField()
