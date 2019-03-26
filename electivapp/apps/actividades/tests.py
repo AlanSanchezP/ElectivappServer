5456e3f43552f7599f02598ffc64a7e296369da1
Alan Sanchez <sanchezpineda03@gmail.com>
2019-03-25 21:00:21 -0600

    Modificado archivo .coveragerc

--- electivapp/apps/actividades/tests.py
+++ electivapp/apps/actividades/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import TipoActividad
 
-# Create your tests here.
+# models test
+class WhateverTest(TestCase):
+
+    def create_whatever(self, nombre="only a test", categoria="IV", horasRequeridas=5):
+        return TipoActividad.objects.create(nombre=nombre, categoria=categoria, horasRequeridas=horasRequeridas)
+
+    def test_whatever_creation(self):
+        w = self.create_whatever()
+        self.assertTrue(isinstance(w, TipoActividad))
