4cf1eb134d80314fe48626858e6fccba5511878b
Alan Sanchez <sanchezpineda03@gmail.com>
2019-04-04 21:26:35 -0600

    Primer test unitario para actividades/views.py

--- electivapp/apps/actividades/tests.py
+++ electivapp/apps/actividades/tests.py
@@ -4,34 +4,62 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 
 from .models import TipoActividad, Actividad
+from .views import RegistrarActividadView
 from electivapp.apps.alumnos.models import Alumno
 
-class Test_TipoActividad(TestCase):
+def createTipoActividad(nombre="Nombre de prueba", categoria="IV", horasRequeridas=1):
+    return TipoActividad.objects.create(
+        nombre=nombre,
+        categoria=categoria,
+        horasRequeridas=horasRequeridas,
+    )
+
+def createAlumno(boleta=2016601609, nombre="Alan Sánchez Pineda", carrera="IN"):
+    return Alumno.objects.create(
+        boleta=boleta,
+        nombre=nombre,
+        carrera=carrera,
+    )
+
+def createActividad(duracion=1, alumno=createAlumno(), tipo=createTipoActividad(), fecha=date.today()):
+    return Actividad.objects.create(
+        duracion=duracion,
+        alumno=alumno,
+        tipo=tipo,
+        fecha=fecha
+    )
+
+class Test_TipoActividad_Model(TestCase):
     def test_horas_mayor_cero(self):
-        obj = TipoActividad.objects.create(
-            nombre="Nombre de prueba",
-            categoria="IV",
-            horasRequeridas=0,
-        )
+        obj = createTipoActividad(horasRequeridas=0)
         self.assertRaises(ValidationError, obj.clean)
 
-class Test_Actividad(TestCase):
+class Test_Actividad_Model(TestCase):
     def test_valor_function(self):
-        tipo = TipoActividad.objects.create(
-            nombre="Nombre de prueba",
-            categoria="IV",
-            horasRequeridas=10,
-        )
-        alumno = Alumno.objects.create(
-            boleta=2016601609,
-            nombre="Alan Sánchez Pineda",
-            carrera="II",
-        )
+        tipo = createTipoActividad(horasRequeridas=10)
+        alumno = createAlumno()
 
-        obj = Actividad.objects.create(
+        obj = createActividad(
             duracion=3.2,
             alumno=alumno,
             tipo=tipo,
-            fecha=date.today()
         )
         self.assertEquals(obj.valor(), 0.32)
+
+class Test_Actividad_Views(TestCase):
+    def test_insertar_actividad(self):
+        obj = RegistrarActividadView()
+        tipo = createTipoActividad()
+        alumnoA = createAlumno()
+        alumnoB = createAlumno(boleta=2016601610)
+
+        # Damos 15 creditos al alumno1 para que
+        # le falte solo uno
+        for i in range(0, 14):
+            actividad = createActividad(tipo=tipo, alumno=alumnoA)
+
+        obj.insertarActividad(alumno=alumnoA, duracion=1, tipo=tipo.id)
+        self.assertEquals(alumnoA.terminado, True)
+
+        obj.insertarActividad(alumno=alumnoB, duracion=1, tipo=tipo.id)
+        self.assertEquals(alumnoB.terminado, False)
