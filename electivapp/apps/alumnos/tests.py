084931df3ac92d9fa325633eaa37d5ca3b3da4ed
Erick Ortega <erickoortegaherrera@gmail.com>
2019-05-22 00:52:34 -0500

    Cambios eventos/api.py

--- electivapp/apps/alumnos/tests.py
+++ electivapp/apps/alumnos/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Alumno
 
+def createAlumno(boleta=2016601609, nombre="Alan Sánchez Pineda", carrera="IN"):
+    return Alumno.objects.create(
+        boleta=boleta,
+        nombre=nombre,
+        carrera=carrera,
+    )
+
+#class Test_Alumno_Model(TestCase):
+#	def test_str(self):
+#		obj = createAlumno()
+#		self.assertEquals(obj.__str__(), obj.nombre)
+#
+#	def test_creditos(self)
+#		obj = createAlumno()
+
+		
+
+		
 # Create your tests here.
